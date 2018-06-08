0ec0390663ab5dcf6f47316b4665b48f3435fc27
hibbahagh <hibbahagh@gmail.com>
2018-06-08 20:17:32 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,7 +7,30 @@ class Dog
 end
 
 
-def name = (name)
-  @name = 
+def name = (dog_name)
+  @name = dog_name 
 end
   
+def breed = (dog_breed)
+  @breed = dog_breed
+end 
+end 
+
+
+  
+#   class Dog 
+   
+#   def name=(dog_name)
+#     @this_dogs_name = dog_name 
+#   end 
+     
+#   def name
+#     @this_dogs_name
+#   end  
+  
+# end
+
+# lassie = Dog.new
+# lassie.name = "Lassie"
+
+# puts lassie.name
