2d4c1b9d17f245db9e4a55496f22607edff2ed15
hibbahagh <hibbahagh@gmail.com>
2018-06-08 20:25:39 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,34 @@
 # dog.rb
+
+class Dog
+  
+  def dog
+  @Dog
+  end
+
+
+  def name=(dog_name)
+    @name = dog_name 
+  end
+  
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end 
+
+  def breed
+   @breed
+  end
+
+  def name 
+    @name
+  end
+
+end 
+
+dog = Dog.new
+  
+dog.name = "Lassie"
+puts dog.name
+
+
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,34 @@
 # person.rb
+
+class Person
+  
+  def person
+  @person
+  end
+
+
+  def name=(person_name)
+    @name = person_name 
+  end
+  
+  def job=(person_job)
+    @job = person_job
+  end 
+
+  def job
+   @job
+  end
+
+  def name 
+    @name
+  end
+
+end 
+
+person = Person.new
+  
+person.name = "Melany"
+puts person.name
+
+
+
