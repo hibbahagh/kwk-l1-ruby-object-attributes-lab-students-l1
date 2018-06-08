555283727fd1f358f1f0b048346da724bd4b6d68
hibbahagh <hibbahagh@gmail.com>
2018-06-08 20:22:50 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -29,19 +29,6 @@ dog = Dog.new
   
 dog.name = "Lassie"
 puts dog.name
-#   class Dog 
-   
-#   def name=(dog_name)
-#     @this_dogs_name = dog_name 
-#   end 
-     
-#   def name
-#     @this_dogs_name
-#   end  
-  
-# end
 
-# lassie = Dog.new
-# lassie.name = "Lassie"
 
-# puts lassie.name
+
