aaaf92e122f4682502c688cf497ac5df8f267d04
hibbahagh <hibbahagh@gmail.com>
2018-06-08 20:24:13 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,34 @@
 # person.rb
+
+class Person
+  
+  def person
+  @person
+  end
+
+
+  def name=(person_name)
+    @name = person_name 
+  end
+  
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end 
+
+  def breed
+   @breed
+  end
+
+  def name 
+    @name
+  end
+
+end 
+
+dog = Dog.new
+  
+dog.name = "Lassie"
+puts dog.name
+
+
+
